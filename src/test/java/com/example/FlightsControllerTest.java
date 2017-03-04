cbbf65d26bf746788670ac51106eff84be05f299
scottyvg <svangilder@gmail.com>
2017-03-03 20:56:38 -0700

    playing around with mocking

---
+++ src/test/java/com/example/FlightsControllerTest.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+/**
+ * Created by scottyvg on 3/3/17.
+ */
+@RunWith(SpringRunner.class)
+@WebMvcTest(FlightsController.class)
+public class FlightsControllerTest {
+    @Autowired
+    MockMvc;
+
+    @Test
+    public void testSomething() throws Exception {
+        when(service.doStuff()).thenReturn("Something...");
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/fligths");
+
+        this.mvc.perform(request).andExpect(content)
+    }
+}
