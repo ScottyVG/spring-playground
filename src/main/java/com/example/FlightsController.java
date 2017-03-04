f0a22ba9109f7a8a67dea5245f2522dab0a25c82
scottyvg <svangilder@gmail.com>
2017-03-03 21:08:44 -0700

    math tests

--- src/main/java/com/example/FlightsController.java
+++ src/main/java/com/example/FlightsController.java
@@ -1,16 +1,16 @@
-package com.example;
-
-import org.springframework.web.bind.annotation.RestController;
-
-/**
- * Created by scottyvg on 3/3/17.
- */
-@RestController
-public class FlightsController {
-
-    private final SuperComplexClass service;
-
-    public FlightsController(SuperComplexClass service) {
-        this.service = service;
-    }
-}
+//package com.example;
+//
+//import org.springframework.web.bind.annotation.RestController;
+//
+///**
+// * Created by scottyvg on 3/3/17.
+// */
+//@RestController
+//public class FlightsController {
+//
+//    private final SuperComplexClass service;
+//
+//    public FlightsController(SuperComplexClass service) {
+//        this.service = service;
+//    }
+//}
--- src/main/java/com/example/SuperComplexClass.java
+++ src/main/java/com/example/SuperComplexClass.java
@@ -1,15 +1,15 @@
-package com.example;
-
-import org.springframework.stereotype.Service;
-
-/**
- * Created by scottyvg on 3/3/17.
- */
-@Service
-public class SuperComplexClass {
-
-    public String doStuff() {
-
-
-    }
-}
+//package com.example;
+//
+//import org.springframework.stereotype.Service;
+//
+///**
+// * Created by scottyvg on 3/3/17.
+// */
+//@Service
+//public class SuperComplexClass {
+//
+//    public String doStuff() {
+//
+//
+//    }
+//}
