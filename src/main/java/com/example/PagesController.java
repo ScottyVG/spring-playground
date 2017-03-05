9cc63283969a6faa83bc6139d3ac48e6b19c05aa
scottyvg <svangilder@gmail.com>
2017-03-05 12:19:10 -0700

    add hello & pages controller

--- src/main/java/com/example/PagesController.java
+++ src/main/java/com/example/PagesController.java
@@ -3,15 +3,13 @@ package com.example;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Created by scottyvg on 3/5/17.
- */
-
 @RestController
-public class HelloController {
+public class PagesController {
 
-    @GetMapping("/")
+    @GetMapping("/hello")
+
     public String helloWorld() {
-        return "Hello from Spring!";
+        return "Hello Scotty!! You are super cool!";
     }
+
 }
